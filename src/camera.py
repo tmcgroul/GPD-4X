230e31a13a65ab97dc8bb1221857cb6863d11bcf
Denis Snagovskiy <denis.snagovskiy@list.ru>
2020-11-05 09:40:22 +0300

    Replace absolute movement to vectors movement in camera class

--- src/camera.py
+++ src/camera.py
@@ -1,56 +1,56 @@
 from src.constants import *
+vec = pg.math.Vector2
 
 
 class Camera:
     def __init__(self, width, height, activate_borders):
-        self.x = self.y = 0
-        self.xv = self.yv = 0
+        self.velocity = vec(0, 0)
+        self.pos = vec(0, 0)
         self.width = width
         self.height = height
         self.activate_borders = activate_borders
 
-        self.camera = pg.Rect(self.x, self.y, self.width, self.height)
+        self.camera = pg.Rect(self.pos.x, self.pos.y, self.width, self.height)
 
     def apply(self, entity):
         return entity.rect.move(self.camera.topleft)
 
     def handle_key_down(self, key):
         if key == CAMERA_SHORTCUTS["Move up"]:
-            self.yv = CAMERA_SPEED
+            self.velocity.y = CAMERA_SPEED
         if key == CAMERA_SHORTCUTS["Move down"]:
-            self.yv = -CAMERA_SPEED
+            self.velocity.y = -CAMERA_SPEED
 
         if key == CAMERA_SHORTCUTS["Move left"]:
-            self.xv = CAMERA_SPEED
+            self.velocity.x = CAMERA_SPEED
         if key == CAMERA_SHORTCUTS["Move right"]:
-            self.xv = -CAMERA_SPEED
+            self.velocity.x = -CAMERA_SPEED
 
         if key == CAMERA_SHORTCUTS["Activate/deactivate borders"]:
             self.activate_borders = not self.activate_borders
 
     def handle_key_up(self, key):
         if key == CAMERA_SHORTCUTS["Move up"]:
-            self.yv = 0
+            self.velocity.y = 0
         if key == CAMERA_SHORTCUTS["Move down"]:
-            self.yv = 0
+            self.velocity.y = 0
 
         if key == CAMERA_SHORTCUTS["Move left"]:
-            self.xv = 0
+            self.velocity.x = 0
         if key == CAMERA_SHORTCUTS["Move right"]:
-            self.xv = 0
+            self.velocity.x = 0
 
     def _scrolling_limit(self):
         """Limit scrolling to map size"""
-        self.x = min(0, self.x)  # left
-        self.y = min(0, self.y)  # top
-        self.x = max(-(self.width - SCREEN_WIDTH), self.x)  # right
-        self.y = max(-(self.height - SCREEN_HEIGHT), self.y)  # bottom
+        self.pos.x = min(0.0, self.pos.x)  # left
+        self.pos.y = min(0.0, self.pos.y)  # top
+        self.pos.x = max(-(self.width - SCREEN_WIDTH), self.pos.x)  # right
+        self.pos.y = max(-(self.height - SCREEN_HEIGHT), self.pos.y)  # bottom
 
     def update(self):
-        self.x += self.xv
-        self.y += self.yv
+        self.pos += self.velocity
 
         if self.activate_borders is True:
             self._scrolling_limit()
 
-        self.camera = pg.Rect(self.x, self.y, self.width, self.height)
+        self.camera = pg.Rect(self.pos.x, self.pos.y, self.width, self.height)
